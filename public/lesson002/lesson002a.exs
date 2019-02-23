04df85fb51106df0ca1330752bba942d9d499e43
Dan Bikle <danbikle@outlook.com>
2019-02-23 13:17:48 -0800

    more

--- public/lesson002/lesson002a.exs
+++ public/lesson002/lesson002a.exs
@@ -1,2 +1,12 @@
 # lesson002a.exs
 
+# This script should lead me towards concurrent programming.
+
+defmodule SpawnMod do
+  def hello1 do
+    IO.puts "Hello 1"
+  end
+end
+
+# This should work:
+SpawnMod.hello1

a1e5446adf97be7c17346c7bfd60c3584b2264df
Dan Bikle <danbikle@outlook.com>
2019-02-24 12:39:00 -0800

    more

--- public/lesson002/lesson002c.exs
+++ public/lesson002/lesson002c.exs
@@ -1,5 +1,22 @@
 # lesson002c.exs
 
-# This script should call spawn()
+# This script should call spawn() which should create a Process.
+# A Process should have an identifier which we call a: "PID".
 
-spawn(SpawnMod, :hello2, [])
+# The spawn/3 function wants these arguments:
+# - Module name
+# - Function name in that Module
+# - List of arguments for the Function
+
+# spawn/3 should return a PID after it spawns a Process
+# from Elixir.SpawnMod.beam:
+
+my_pid = spawn(SpawnMod, :hello2, [])
+
+# I should see the PID.
+
+# This fails:
+# IO.puts my_pid
+
+# This should show the PID:
+IO.puts(inspect(my_pid))

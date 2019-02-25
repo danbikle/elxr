5b1e1057dc661c626e8d6abeb21b922658c6fb1d
Dan Bikle <danbikle@outlook.com>
2019-02-24 22:22:49 -0800

    more

--- public/lesson002/bookdemo.ex
+++ public/lesson002/bookdemo.ex
@@ -1,26 +1,37 @@
-#---
-# Excerpted from "Programming Elixir 1.6",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/elixir16 for more book information.
-#---
-defmodule Spawn1 do
-  def greet do
+
+defmodule MyMsgMod do
+  def my_rcv do
+    # I should wait for a msg:
     receive do
       {sender, msg} ->
-        send sender, { :ok, "Hello, #{msg}" }
+        IO.puts inspect(self())
+        IO.puts "received: "
+	IO.puts inspect(msg)
+	IO.puts "From sender:"
+	IO.puts inspect(sender)
+	# I should acknowledge the msg:
+	msg2_s = "I got your msg."
+        send sender, {self(), msg2_s}
+    end
+  end
+
+  def my_sndr do
+    pid = spawn(MyMsgMod, :my_rcv, [])
+    # I should send a message:
+    msg1_s = "Sell FB!, Sell AAPL!"
+    send pid, {self(), msg1_s}
+    # I should receive acknowledgement:
+    receive do
+      {acksender, message} ->
+      IO.puts "Then, "
+      IO.puts inspect(acksender)
+      IO.puts "Says: "
+      IO.puts message
     end
   end
 end
 
-# here's a client
-pid = spawn(Spawn1, :greet, [])
-send pid, {self(), "World!"}
+# drum roll:
+MyMsgMod.my_sndr
 
-receive do
-  {:ok, message} ->
-    IO.puts message
-end
-  
+

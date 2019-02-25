6491a2eb22031da544328d6f2af67255b4f51e19
Dan Bikle <danbikle@outlook.com>
2019-02-25 13:32:14 -0800

    more

--- public/lesson002/bookdemo.ex
+++ public/lesson002/bookdemo.ex
@@ -3,15 +3,15 @@ defmodule MyMsgMod do
   def my_rcv do
     # I should wait for a msg:
     receive do
-      {sender, msg} ->
+      {sender_pid, msg} ->
         IO.puts inspect(self())
         IO.puts "received: "
 	IO.puts inspect(msg)
-	IO.puts "From sender:"
-	IO.puts inspect(sender)
+	IO.puts "From sender_pid:"
+	IO.puts inspect(sender_pid)
 	# I should acknowledge the msg:
 	msg2_s = "I got your msg."
-        send sender, {self(), msg2_s}
+        send sender_pid, {self(), msg2_s}
     end
   end
 
@@ -22,9 +22,9 @@ defmodule MyMsgMod do
     send pid, {self(), msg1_s}
     # I should receive acknowledgement:
     receive do
-      {acksender, message} ->
+      {acksender_pid, message} ->
       IO.puts "Then, "
-      IO.puts inspect(acksender)
+      IO.puts inspect(acksender_pid)
       IO.puts "Says: "
       IO.puts message
     end

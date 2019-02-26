a2d825c4f105f0e94e5ece9f2498888d0e909cb4
Dan Bikle <danbikle@outlook.com>
2019-02-26 14:18:42 -0800

    more

--- public/lesson003/ur_msg_mod1.ex
+++ public/lesson003/ur_msg_mod1.ex
@@ -19,7 +19,8 @@
 
 defmodule UrMsgMod1 do
   def ur_rcv do
-    # I should wait for a msg:
+  
+    # I should wait for first msg:
     receive do
       {sender_pid, msg} ->
         IO.puts inspect(self())
@@ -28,14 +29,29 @@ defmodule UrMsgMod1 do
 	IO.puts "From sender_pid:"
 	IO.puts inspect(sender_pid)
 	# I should acknowledge the msg:
-	msg2_s = "I got your msg."
+	msg2_s = "I got your first msg."
         send sender_pid, {self(), msg2_s}
     end
+  
+    # I should wait for second msg:
+    receive do
+      {sender_pid, msg} ->
+        IO.puts inspect(self())
+        IO.puts "received: "
+	IO.puts inspect(msg)
+	IO.puts "From sender_pid:"
+	IO.puts inspect(sender_pid)
+	# I should acknowledge the msg:
+	msg2_s = "I got your second msg."
+        send sender_pid, {self(), msg2_s}
+    end
+    
   end
 
   def ur_sndr do
     pid = spawn(UrMsgMod1, :ur_rcv, [])
-    # I should send a message:
+
+    # I should send first message:
     msg1_s = "Sell IBM!!"
     send pid, {self(), msg1_s}
     # I should receive acknowledgement:
@@ -46,5 +62,18 @@ defmodule UrMsgMod1 do
       IO.puts "Says: "
       IO.puts message
     end
+
+    # I should send second message:
+    msg2_s = "Sell more IBM!!"
+    send pid, {self(), msg2_s}
+    # I should receive acknowledgement:
+    receive do
+      {acksender_pid, message} ->
+      IO.puts "Then, "
+      IO.puts inspect(acksender_pid)
+      IO.puts "Says: "
+      IO.puts message
+    end
+    
   end
 end

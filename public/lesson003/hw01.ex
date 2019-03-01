fc449f5118283e3853f65c46fe5b9d62df484cc6
Dan Bikle <danbikle@outlook.com>
2019-03-01 14:24:53 -0800

    more

--- public/lesson003/hw01.ex
+++ public/lesson003/hw01.ex
@@ -1,2 +1,25 @@
 # hw01.ex
 
+# This script should help me do hw in lesson003.
+
+# Demo:
+# elixir hw01.ex
+
+defmodule Hw01 do
+  # This module should hold two functions
+  
+  def fn01 do
+    receive do
+      {pid, "I ama string"} -> "hello"
+    end
+    "I am fn01"
+  end
+  
+  def fn02 do
+    "I am fn02"
+  end
+    
+end
+
+# I should call the first function:
+Hw01.fn01

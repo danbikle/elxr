fb66e08b72efb7820c29372e33caa420a32acf12
Dan Bikle <danbikle@outlook.com>
2019-03-01 16:27:39 -0800

    more

---
+++ public/lesson003/hw3f.ex
@@ -0,0 +1,26 @@
+# hw3f.ex
+
+# This script should:
+# have a module that holds two functions.
+# Demo:
+# elixir hw3f.ex
+
+defmodule Hw3f do
+
+  # This module should hold two functions.
+  
+  def fn1 do
+    # This function should have a receive-block.
+    receive do
+      # This receive-block should match a tuple with a pid and some_string.
+      # The function-body should be: "hello"
+      {pid, some_string} -> "hello" 
+    end
+    
+  end
+
+  def fn2 do
+    # This function should do nothing.  
+  end
+
+end

8ca565b2589e6b4fe65fc012630d64eea0bc2a5c
Dan Bikle <danbikle@outlook.com>
2019-03-01 16:29:43 -0800

    more

---
+++ public/lesson003/hw3g.ex
@@ -0,0 +1,30 @@
+# hw3g.ex
+
+# This script should:
+# have a module that holds two functions.
+# Below the module, a line should call the first function.
+# Demo:
+# elixir hw3g.ex
+
+defmodule Hw3g do
+
+  # This module should hold two functions.
+  
+  def fn1 do
+    # This function should have a receive-block.
+    receive do
+      # This receive-block should match a tuple with a pid and some_string.
+      # The function-body should be: "hello"
+      {pid, some_string} -> "hello" 
+    end
+    
+  end
+
+  def fn2 do
+    # This function should do nothing.  
+  end
+
+end
+
+# I should call the first function:
+Hw3g.fn1
